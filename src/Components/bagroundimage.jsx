a9a062d7a7eb88e3e1fec395241f8c554b8768c0
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-02-05 02:03:57 +0530

    user inter face is done

---
+++ src/Components/bagroundimage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './passwordGenerator.css';
+
+function Bagroundimage() {
+  return (
+    <div className='upload-vector'></div>
+  )
+}
+
+export default Bagroundimage;
